52aceb63b83333bef173332b946e351cf0a6edd6
alexliap <alexandrosliapates@gmail.com>
2025-01-16 21:43:13 +0200

    finished option exercises

---
+++ src/section_5/option/problem_2.rs
@@ -0,0 +1,25 @@
+// Problem 2: Fix the code so that it compiles.
+
+// FIXED
+
+fn check_fruit(input_fruit: String) -> Option<String> {
+    let fruit_basket = vec![
+        String::from("mango"),
+        String::from("apple"),
+        String::from("banana"),
+    ];
+    for fruit in fruit_basket {
+        if input_fruit == fruit {
+            return Some(fruit);
+        }
+    }
+    None
+}
+
+fn main() {
+    let user_fruit = String::from("apple");
+    if let Some(fruit) = check_fruit(user_fruit) {
+        // this is something like "if x is not None" in Python, maybe ...
+        println!("User's name: {fruit}");
+    }
+}
